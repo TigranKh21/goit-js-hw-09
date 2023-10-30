6743bc3078a2f2e3039724826d4f82ce554b1fd6
TigranKh21 <tigran.khachaturyants@gmail.com>
2023-10-30 10:49:00 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+const bodyEl = document.querySelector('body')
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+function onChangeColor(){
+    bodyEl.style.backgroundColor = getRandomHexColor()
+}
+
+let timerId = null;
+startBtn.addEventListener('click', (event)=>{
+    event.preventDefault();
+    timerId = setInterval(onChangeColor, 1000);
+    startBtn.setAttribute('disabled', 'true');
+    stopBtn.removeAttribute('disabled');
+
+})
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    stopBtn.setAttribute('disabled', 'true');
+    startBtn.removeAttribute('disabled');
+})
