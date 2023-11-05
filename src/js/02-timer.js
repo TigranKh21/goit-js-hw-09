569022a7ef035b43f4dee5985f41b14856692377
TigranKh21 <tigran.khachaturyants@gmail.com>
2023-11-05 20:15:25 +0200

    task-2-complete

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,23 +1,71 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
 const dateInputEl = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
-const timeParamsEl = document.querySelectorAll('.value')
-const attr = timeParamsEl
-//attrNew = attr.map(({span}) => {return span})
+const timeParamsEl = document.querySelectorAll('.value');
 
-flatpickr(dateInputEl);
-startBtn.addEventListener('click', onCountdown)
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    compareDates(selectedDates[0]);
+  },
+};
 
-function onCountdown(event){
-    const myTime = new Date (dateInputEl.value).getTime()
-    console.log(myTime);
-    const now = new Date().getTime()
-    const intervalId = setInterval(()=>{
-        countDownTime = myTime - now - 1000;
-        //myTime = myTime - 1000;
-    }, 1000)
-    console.log(intervalId);
+startBtn.setAttribute('disabled', 'true');
+
+function compareDates(selectedDates) {
+  const now = new Date();
+  if (now < selectedDates) {
+    startBtn.removeAttribute('disabled');
+  } else {
+    startBtn.setAttribute('disabled', 'true');
+    window.alert('Please choose a date in the future');
+  }
 }
 
+flatpickr(dateInputEl, options);
+
+startBtn.addEventListener('click', onStartBtn);
+
+function onStartBtn() {
+  const intervalId = setInterval(() => {
+    const futureTime = new Date(dateInputEl.value).getTime();
+    startBtn.setAttribute('disabled', 'true');
+    const now = new Date().getTime();
+    leftTime = convertMs(futureTime - now);
+
+    timeParamsEl[0].textContent =
+      leftTime.days >= 10 ? leftTime.days : '0' + leftTime.days;
+    timeParamsEl[1].textContent =
+      leftTime.hours >= 10 ? leftTime.hours : '0' + leftTime.hours;
+    timeParamsEl[2].textContent =
+      leftTime.minutes >= 10 ? leftTime.minutes : '0' + leftTime.minutes;
+    timeParamsEl[3].textContent =
+      leftTime.seconds >= 10 ? leftTime.seconds : '0' + leftTime.seconds;
+
+    console.log(futureTime - now);
+
+    if (futureTime - now <= 999) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
